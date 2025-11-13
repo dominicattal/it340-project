6fb44db3ddf418d3c38f11111374bdf62dfe65bb
dominicattal <attalientidominic@gmail.com>
2025-11-13 11:16:58 -0500

    merged login and home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
+import { Home } from './home/home';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
     {
         path: 'login',
         component: Login
