077670a2c13acd0c23ff7ae8407ad8fc7530f469
dominicattal <attalientidominic@gmail.com>
2025-12-03 21:22:42 -0500

    backend is done

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -7,21 +7,36 @@ function validateUsername(username) {
     return true;
 }
 
-router.post('/register', async (req, res) => {
-    const username = req.body["username"]
-    const password = req.body["password"]
-    if (!username) {
-        res.status(400).json({"message": "no username"})
-    }
-    if (!password) {
-        res.status(400).json({"message": "no password"})
-    }
+router.post('/register', async (req, res) => {    
     try {
-        let hashed = hash({password})
+        const username = req.body["username"]
+        const password = req.body["password"]
+        let hashed = hash(password)
+        const existingUser = await Login.findOne({username})
+        console.log(existingUser)
+        if (existingUser)
+            throw new Error("user already exists")
         const user = await Login.create({username, password:hashed})
         res.status(200).json({
             "message": "successfully created user"
         })
+    } catch (error) {
+        console.log(error.message)
+        res.status(400).json({error: error.message})
+    }
+})
+
+router.get('/login', async (req, res) => {
+    try {
+        const username = req.body["username"]
+        const password = req.body["password"]
+        let hashed = hash(password)
+        const user = await Login.findOne({ username })
+        if (!user)
+            throw new Error("user does not exist")
+        if (user["password"] != hashed)
+            throw new Error("Incorrect password")
+        res.status(200).json(user)
     } catch (error) {
         console.log(error.message)
         res.status(400).json({error: error.message})
