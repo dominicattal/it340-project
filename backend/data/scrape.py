42da11777e0a83d2ca3a2324412b5a1dc323bbc1
dominicattal <attalientidominic@gmail.com>
2025-12-18 15:58:05 -0500

    gundamplanet

--- backend/data/scrape.py
+++ backend/data/scrape.py
@@ -58,8 +58,30 @@ def usagundam():
                 price_str = model.find("s").text.strip()
                 price = float(price_str[2:])
                 write_output(grade, name, img_link, base_url, link, price)
+def gundamplanet():
+    files = [
+        ("gundamplanet-eg.html", "EG"),
+        ("gundamplanet-hg.html", "HG"),
+        ("gundamplanet-rg.html", "RG"),
+        ("gundamplanet-mg.html", "MG"),
+        ("gundamplanet-pg.html", "PG")
+    ]
+    base_url = "https://www.gundamplanet.com/"
+    for filename, grade in files:
+        with open(filename, "r", encoding="utf8") as txtfile:
+            html_doc = txtfile.read()
+            soup = BeautifulSoup(html_doc, "html.parser")
+            models = soup.find_all(class_="preview-card")
+            for model in models:
+                link = model.find("a", class_="product-detail-link")["href"]
+                name = model.find("div", class_="preview-card-hovertext-title").text.strip()
+                img_link = model.find("img")["src"]
+                price_str = model.find("span", class_="price-item").text.strip()
+                price = float(price_str[price_str.index("$")+1:])
+                write_output(grade, name, img_link, base_url, link, price)
 newtype()
 usagundam()
+gundamplanet()
 output_file.write("\t\tnull\n")
 output_file.write("\t]\n")
 output_file.write("}\n")
